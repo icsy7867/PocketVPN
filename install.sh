3d871a07e3b6219f40dca37931757e232f4be65b
ICANSEEYOU7687 <wstewart3@gmail.com>
2018-11-20 16:14:19 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
 echo 'Installing apache and dependencies'
-.website/install.sh
+./website/install.sh
+
+echo ''
+echo ''
 
 echo 'Installing hostapd, copying config files and setting up NAT'
-.hostap/install.sh
+./hostap/install.sh
+
+echo ''
+echo ''
 
 sleep 100
 
 echo 'Complete!'
-echo 'Please run'
+sleep 100
+echo 'Please run echo 1 > /proc/sys/net/ipv4/ip_forward as root if you have not done so already'
