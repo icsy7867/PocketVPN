92385c8f40ea2638e0a1b333677d1d8d13d15a7f
ICANSEEYOU7687 <wstewart3@gmail.com>
2018-11-20 13:46:50 -0500

    Update install.sh

--- hostap/install.sh
+++ hostap/install.sh
@@ -14,6 +14,7 @@ cp resolv.dnsmasq.conf /etc/resolv.dnsmasq.conf
 cp Channel_Select.sh /var/www/html/scripts/Channel_Select.sh
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
+sysctl -w net.ipv4.ip_forward=1
 
 #brctl addbr br0
 #brctl addif br0 eth0
