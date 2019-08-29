e4fe96318c2223d31f29d8486ea6826ec23b021c
icsy7867 <wstewart3@gmail.com>
2019-08-29 16:54:10 -0400

    Update install_kodi.sh

--- install_kodi.sh
+++ install_kodi.sh
@@ -1,9 +1,7 @@
 apt-get install kodi
 cp /usr/share/applications/kodi.desktop /etc/xdg/autostart/z-kodi.desktop
 
-wget https://raw.githubusercontent.com/zjoasan/netflix-install-script/master/netflix_prep_install.sh
-chmod +x netflix_prep_install.sh
-./netflix_prep_install.sh
+wget -q https://github.com/CastagnaIT/plugin.video.netflix/archive/master.zip
 
 wget https://github.com/MediaBrowser/Emby.Releases/releases/download/4.2.1.0/emby-server-deb_4.2.1.0_armhf.deb
 dpkg -i emby-server-deb_4.2.1.0_armhf.deb
