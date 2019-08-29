cda8a8adb4cd28fe18549a42e9e4d39d23407552
icsy7867 <wstewart3@gmail.com>
2019-08-29 16:58:02 -0400

    Update install_kodi.sh

--- install_kodi.sh
+++ install_kodi.sh
@@ -2,6 +2,11 @@ apt-get install kodi
 cp /usr/share/applications/kodi.desktop /etc/xdg/autostart/z-kodi.desktop
 
 wget -q https://github.com/CastagnaIT/plugin.video.netflix/archive/master.zip
+sudo apt-get install -q python-crypto
+sudo pip install -q -U setuptools
+sudo pip install -q wheel
+sudo pip install -q pycryptodomex==3.8.2
+
 
 wget https://github.com/MediaBrowser/Emby.Releases/releases/download/4.2.1.0/emby-server-deb_4.2.1.0_armhf.deb
 dpkg -i emby-server-deb_4.2.1.0_armhf.deb
