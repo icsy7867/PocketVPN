9158e43043e96703474c01d8932c5e26f7e2acbb
icsy7867 <wstewart3@gmail.com>
2019-08-29 16:45:36 -0400

    Update install_kodi.sh

--- install_kodi.sh
+++ install_kodi.sh
@@ -1,3 +1,5 @@
 apt-get install kodi
-
 cp /usr/share/applications/kodi.desktop /etc/xdg/autostart/z-kodi.desktop
+
+wget https://github.com/MediaBrowser/Emby.Releases/releases/download/4.2.1.0/emby-server-deb_4.2.1.0_armhf.deb
+dpkg -i emby-server-deb_4.2.1.0_armhf.deb
