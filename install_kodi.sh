a67719c02ffcb355452e553af4b79e40ac375688
icsy7867 <wstewart3@gmail.com>
2019-08-30 08:48:41 -0400

    Update install_kodi.sh

--- install_kodi.sh
+++ install_kodi.sh
@@ -4,6 +4,8 @@ cp /usr/share/applications/kodi.desktop /etc/xdg/autostart/z-kodi.desktop
 wget -q https://github.com/CastagnaIT/plugin.video.netflix/archive/master.zip
 sudo apt-get install -q python-crypto
 apt-get install -q -y build-essential python-pip libnss3 libnspr4
+sudo apt install python-all-dev python-setuptools python-wheel -y
+sudo apt install python-crypto-dbg python-crypto-doc python-pip-whl -y
 
 sudo pip install -q -U setuptools
 sudo pip install -q wheel
