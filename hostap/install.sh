77c8513c37c738a488ffa99bed81b61f50975b73
icsy7867 <wstewart3@gmail.com>
2019-08-30 10:32:12 -0400

    Update install.sh

--- hostap/install.sh
+++ hostap/install.sh
@@ -14,7 +14,10 @@ cp resolv.dnsmasq.conf /etc/resolv.dnsmasq.conf
 cp Channel_Select.sh /var/www/html/scripts/Channel_Select.sh
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
-sysctl -w net.ipv4.ip_forward=1
+bash -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
+
+#sysctl -w net.ipv4.ip_forward=1
+
 ip addr del 169.254.47.93/16 dev wlan0
 
 #brctl addbr br0
