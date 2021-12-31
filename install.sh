c42db387b47912682ab391653d91c6e0e95c06e3
wstewart3@gmail.com <wstewart3@gmail.com>
2021-12-31 17:14:39 -0500

    Working dperson openvpn test

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 ###Create PocketVPN Directories
 mkdir -p /etc/PocketVPN/openvpn/config
-mv ./pocketvpn-web/website/openvpn/config /etc/PocketVPN/openvpn/
-touch /etc/PocketVPN/openvpn/auth
+cp -R ./openvpn_config_template/. /etc/PocketVPN/openvpn/config
+
+if [ ! -f /etc/PocketVPN/openvpn/auth ]
+then
+    touch /etc/PocketVPN/openvpn/auth
+else
+    echo "Auth File exists"
+fi
 
 chown -R root:root /etc/PocketVPN/openvpn
-chown -R 33:root /etc/PocketVPN/openvpn/config/*
+chown -R 33:root /etc/PocketVPN/openvpn/config
 chown 33:root /etc/PocketVPN/openvpn/auth
 chown 33:root /etc/PocketVPN/openvpn/config
--- pocketvpn-web/docker-run.sh
+++ pocketvpn-web/docker-run.sh
@@ -4,5 +4,9 @@ docker run -d -it -p 8081:80 --name pocketvpn-web \
  -v "/sys/class/thermal/thermal_zone0/temp:/sys/class/thermal/thermal_zone0/temp:ro" \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -v "/etc/PocketVPN/openvpn/config:/var/www/html/openvpn/config" \
+ -v "/etc/PocketVPN/openvpn/auth:/var/www/html/openvpn/auth" \
  --privileged apachetest
 
+
+#docker run -it --cap-add=NET_ADMIN --device /dev/net/tun --name vpn -v "/etc/PocketVPN/openvpn/config/TorGuard.US-Inline.ovpn:/vpn/vpn.conf" 
+# -v "/etc/PocketVPN/openvpn/auth:/auth" -d dperson/openvpn-client
